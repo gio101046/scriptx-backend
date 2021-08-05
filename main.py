669b94c6e802fc675e6f09b572e6fa18e9c288ac
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2021-08-05 19:39:45 -0400

    feat: add ping endpoint

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from api import user_router
 from api import subscription_router
 
 app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
+
 app.include_router(user_router)
 app.include_router(subscription_router)
 
